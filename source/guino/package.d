de7b46d6cdc12844c6d00a0894418db29ef702b1
Andrea Fontana <trikkuz@gmail.com>
2023-10-17 09:28:14 +0200

    formatting

--- source/guino/package.d
+++ source/guino/package.d
@@ -5,11 +5,10 @@ import std.string;
 import std.conv : to;
 
 // Window size hints
-enum WEBVIEW_HINT_NONE=0;  /// Width and height are default size
-enum WEBVIEW_HINT_MIN=1;   /// Width and height are minimum bounds
-enum WEBVIEW_HINT_MAX=2;   /// Width and height are maximum bounds
-enum WEBVIEW_HINT_FIXED=3; /// Window size can not be changed by a user
-
+enum WEBVIEW_HINT_NONE  =  0;    /// Width and height are default size
+enum WEBVIEW_HINT_MIN   =  1;    /// Width and height are minimum bounds
+enum WEBVIEW_HINT_MAX   =  2;    /// Width and height are maximum bounds
+enum WEBVIEW_HINT_FIXED =  3;    /// Window size can not be changed by a user
 
 /// The main struct
 struct WebView {
